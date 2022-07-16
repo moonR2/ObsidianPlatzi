c5cb6803675658450ca49576e1d56b65804fad34
moonR2 <moonyneverend@gmail.com>
2022-07-16 14:19:48 -0500

    pluging settings done

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -3,6 +3,8 @@ import { App, PluginSettingTab, Setting} from 'obsidian'
 import ObsidianPlatzi from 'src/main'
 import {FolderSuggest} from './suggesters/FolderSuggester'
 
+const repo = 'https://github.com/moonR2/ObsidianPlatzi'
+
 export enum FrontMatterType {
 	snakeCase = 'Snake Case',
 	camelCase = 'Camel Case'
@@ -40,6 +42,7 @@ export class ObsidianPlatziSettingsTab extends PluginSettingTab {
 
 		containerEl.createEl('h2', { text: 'General Settings' });
 
+
 		new Setting(containerEl)
 			.setName('New file location')
 			.setDesc('New platzi course notes will be placed here.')
@@ -56,6 +59,58 @@ export class ObsidianPlatziSettingsTab extends PluginSettingTab {
             this.plugin.saveSettings();
           });
 			})
+		
+			containerEl.createEl('h2', { text: 'Frontmatter Settings' });
+
+			new Setting(containerEl)
+			.setName('Frontmatter key type')
+			.addDropdown((dropDown) => {
+				dropDown.addOption(FrontMatterType.snakeCase, FrontMatterType.snakeCase.toString());
+				dropDown.addOption(FrontMatterType.camelCase, FrontMatterType.camelCase.toString());
+				dropDown.setValue(this.plugin.settings.defaultFrontMatterType)
+				dropDown.onChange(async value  => {
+					this.plugin.settings.defaultFrontMatterType = value as FrontMatterType;
+					await this.plugin.saveSettings();
+				});
+			});
+
+			new Setting(containerEl)
+			.setName('Text to insert into frontmatter')
+			.setDesc(createSyntaxesDescription())
+			.addTextArea(textArea => {
+				const prevOption = this.plugin.settings.content;
+				textArea.setValue(prevOption).onChange(async value => {
+					const newOption = value;
+					this.plugin.settings.content = newOption;
+					await this.plugin.saveSettings();
+				})
+			})
+
+			new Setting(containerEl)
+			.setName('Text to insert into content')
+			.setDesc(createSyntaxesDescription())
+			.addTextArea(textArea => {
+				const prevOption = this.plugin.settings.content;
+				textArea.setValue(prevOption).onChange(async value => {
+					const newOption = value;
+					this.plugin.settings.content = newOption;
+					await this.plugin.saveSettings();
+				})
+			})
 			
+			function createSyntaxesDescription(){
+				const fragment = document.createDocumentFragment();
+				fragment.append(
+					'Only the following syntaxes are available: ',
+					fragment.createEl('br'),
+					fragment.createEl('code', {text: '{{title}}'}),
+					fragment.createEl('code', {text: '{{author}}'}),
+					fragment.createEl('code', {text: '{{url}}'}),
+					fragment.createEl('code', {text: '{{coverUrl}}'}),
+					fragment.createEl('code', {text: '{{status}}'}),
+					fragment.createEl('code', {text: '{{rating}}'}),
+				)
+				return fragment;
+			}
 	}
 }
